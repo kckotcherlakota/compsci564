64d872db500372e546434262aa0fbef64131f7c9
kckotcherlakota <37008639+kckotcherlakota@users.noreply.github.com>
2020-02-20 21:18:35 -0600

    Add files via upload

---
+++ createtab.sql
@@ -0,0 +1,54 @@
+DROP TABLE IF EXISTS Items;
+DROP TABLE IF EXISTS Categories;
+DROP TABLE IF EXISTS Bid;
+DROP TABLE IF EXISTS Seller;
+DROP TABLE IF EXISTS Bidder;
+
+CREATE TABLE Items(
+    ItemID INTEGER,
+    Name TEXT,
+    Currently REAL,
+    First_Bid REAL,
+    Buy_Price REAL,
+    Number_of_Bids INTEGER,
+    Started TEXT,
+    Ends TEXT,
+    Seller_UserID TEXT,
+    Description TEXT,
+    PRIMARY KEY(ItemID),
+    FOREIGN KEY(Seller_UserID) REFERENCES Users(UserID) DEFERRABLE INITIALLY DEFERRED,
+    CHECK (Ends > Started)
+);
+
+CREATE TABLE Categories (
+    ItemID INTEGER,
+    Category TEXT,
+    PRIMARY KEY(ItemID, Category),
+    FOREIGN KEY(ItemID) REFERENCES Items(ItemID) DEFERRABLE INITIALLY DEFERRED
+);
+
+CREATE TABLE Bid(
+    ItemID INTEGER;
+    UserID INTEGER;
+    Amount REAL;
+    PRIMARY KEY(ItemID,UserID,Amount);
+    UNIQUE(ItemID,UserID);
+    FOREIGN KEY(ItemID) REFERENCES Items(ItemID) DEFERRABLE INITIALLY DEFERRED,
+    FOREIGN KEY(UserID) REFERENCES Users(UserID) DEFERRABLE INITIALLY DEFERRED
+);
+
+CREATE TABLE Seller(
+    UserID TEXT;
+    Rating INTEGER;
+    PRIMARY KEY(UserID);
+    FOREIGN KEY(ItemID) REFERENCES Items(ItemID) DEFERRABLE INITIALLY DEFERRED
+);
+
+CREATE TABLE Bidder(
+    UserID TEXT;
+    Rating INTEGER;
+    Location TEXT;
+    Country TEXT;
+    PRIMARY KEY(UserID);
+    FOREIGN KEY(ItemID) REFERENCES Items(ItemID) DEFERRABLE INITIALLY DEFERRED
+);
